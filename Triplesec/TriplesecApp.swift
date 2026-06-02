0940801c3d40ed2b91d4ebbd6550f6ced0911f3b
Dj Isaac <contact@dejaydev.com>
2026-06-02 14:28:32 -0700

    Initial Commit

---
+++ Triplesec/TriplesecApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriplesecApp.swift
+//  Triplesec
+//
+//  Created by Dj Isaac on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TriplesecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
